6871b5f9c0b04df0bae3b8bc02efda145a97b1e2
otter <eypk9673@naver.com>
2022-07-11 20:56:55 +0900

    [UPDATE] 중복 파일 삭제 및 함수형으로 변경

--- export_edit.py
+++ export_edit.py
@@ -4,55 +4,64 @@ import zipfile
 import os
 import re
 
-path_dir = './export'
-file_list = os.listdir(path_dir)
-
-# Notion에서 export한 zip파일들을 압축 해제
-for file in file_list:
-    zipfile.ZipFile(f'./export/{file}').extractall('./unzip')
-
-# 압축 해제한 파일들
-unzip_list = os.listdir('./unzip')
-for unzip_file in unzip_list:
-    regex = re.compile('.md$')
-    # MD 파일의 경우
-    if regex.search(unzip_file):
-        try:
-            with open(f'./unzip/{unzip_file}', 'r', encoding='utf-8') as before_file:
-                # 파일을 읽어서 첫번째 줄을 제목 및 파일이름으로 설정
-                lines = before_file.readlines()
-                title = lines[0]
-                file_name = title[2:].rstrip()
-                # 이미 작업이 완료된 파일
-                if unzip_file[:-3] == file_name:
-                    continue
-                # 이미지 경로 퍼센트 인코딩
-                percent_encoding_file = parse.quote(unzip_file)[:-3]
-                title_encoding = parse.quote(file_name)
-
-                # 수정이 필요한 이미지 경로 수정
-                for i in range(len(lines)):
-                    if percent_encoding_file in lines[i]:
-                        lines[i] = lines[i].replace(percent_encoding_file, title_encoding)
-
-                # 새로운 MD 파일
-                with open(f'./unzip/{file_name}.md', 'w', encoding='utf-8') as new_file:
-                    new_file.writelines(lines)
-                # 이미지 폴더명 변경
-                os.rename(f'./unzip/{unzip_file[:-3]}', f'./unzip/{file_name}')
-            # 수정전 MD 파일 삭제
-            os.remove(f'./unzip/{unzip_file}')
-
-        except:
-            with open(f'./unzip/{unzip_file}', 'r', encoding='utf-8') as file:
-                file_title = file.readline()[2:]
-            # 이미 작업이 완료된 파일 및 폴더
-            if unzip_file[:-3] != file_title:
-                os.remove(f'./unzip/{unzip_file}')
-                shutil.rmtree(f'./unzip/{unzip_file[:-3]}')
+class NotionJekyll:
+    def __init__(self):
+        self.path_dir = './export'
+        self.file_list = os.listdir(self.path_dir)
+        self.unzipFile()
+        self.modifyMD()
 
 
+    def unzipFile(self):
+        # Notion에서 export한 zip파일들을 압축 해제
+        for file in self.file_list:
+            regex = re.compile('.zip$')
+            if regex.search(file):
+                zipfile.ZipFile(f'./export/{file}').extractall('./unzip')
 
 
+    def modifyMD(self):
+        # 압축 해제한 파일들
+        self.unzip_list = os.listdir('./unzip')
+        for unzip_file in self.unzip_list:
+            regex = re.compile('.md$')
+            # MD 파일의 경우
+            if regex.search(unzip_file):
+                with open(f'./unzip/{unzip_file}', 'r', encoding='utf-8') as before_file:
+                    # 파일을 읽어서 첫번째 줄을 제목 및 파일이름으로 설정
+                    lines = before_file.readlines()
+                    title = lines[0]
+                    file_name = title[2:].rstrip()
+                    # 이미 작업이 완료된 파일
+                    if unzip_file[:-3] == file_name:
+                        continue
+                    # 이미지 경로 퍼센트 인코딩
+                    percent_encoding_file = parse.quote(unzip_file).split('.')[0]
+                    title_encoding = parse.quote(file_name)
+
+                    # 수정이 필요한 이미지 경로 수정
+                    for i in range(len(lines)):
+                        if percent_encoding_file in lines[i]:
+                            lines[i] = lines[i].replace(percent_encoding_file, title_encoding)
+                self.modifyDir(unzip_file, file_name, lines)
 
 
+    def modifyDir(self, before_file, modify_name, lines):
+        # 이미지 폴더명 변경
+        if os.path.exists(f'./unzip/{before_file[:-3]}'):
+            if os.path.exists(f'./unzip/{modify_name}'):
+                shutil.rmtree(f'./unzip/{modify_name}')
+            os.rename(f'./unzip/{before_file[:-3]}', f'./unzip/{modify_name}')
+
+        # 수정전 MD 파일 삭제
+        if os.path.exists(f'./unzip/{before_file}'):
+            os.remove(f'./unzip/{before_file}')
+    
+        if os.path.exists(f'./unzip/{modify_name}.md'):
+            os.remove(f'./unzip/{modify_name}.md')
+
+        # 새로운 MD 파일
+        with open(f'./unzip/{modify_name}.md', 'w', encoding='utf-8') as new_file:
+            new_file.writelines(lines)
+
+NotionJekyll()
